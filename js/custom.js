3fb214e1b5aa13a77c99ba4080d41e94c19190c7
Olexandr-Sh <shevtcov07@ukr.net>
2021-01-30 14:15:12 +0200

    added program which calculate the volume of the cylinder

---
+++ js/custom.js
@@ -0,0 +1,3 @@
+var height = prompt('enter the height of the cylinder');
+var radius = prompt('enter the radius of the cylinder');
+alert('Cylinder volume with height ' + height + 'sm and radius ' + radius + 'sm is equal ' + Math.PI * (radius * radius) * height);
